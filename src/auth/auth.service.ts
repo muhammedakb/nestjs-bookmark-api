156a51cbb905a2ccb079ccdb020768ba9fa6a970
Muhammet Akbulut <muhammetakb68@gmail.com>
2024-09-01 18:59:58 +0300

    feat: sign in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -38,7 +38,25 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return 'I am signin in';
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    // if user does not exist throw exception
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if password incorrect throw exception
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    // send back the user
+    delete user.hash;
+    return user;
   }
 }
