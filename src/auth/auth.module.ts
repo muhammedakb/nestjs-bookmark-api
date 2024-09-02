f2c7b56edbca580237958fb16127416dd527e500
Muhammet Akbulut <muhammetakb68@gmail.com>
2024-09-02 23:33:50 +0300

    feat: passport js & jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtStrategy } from './strategy';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
